b306747b662fc4f20bbc3954a5f425ac68bf7d71
soysan <soy2.san@gmail.com>
2022-12-28 19:39:36 +0900

    hello test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/soysan/go-practice
+
+go 1.19
---
+++ hello.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+// Named return value
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Printf(Hello("world", ""))
+}
+
+// TDD
+// 1. Write a Test
+// 2. Make sure passing a compiler
+// 3. Check the result of test
+// 4. Fix code for test or function
+// 5. Refactoring.
