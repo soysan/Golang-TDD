9646621b652b2582069b2dd26cad72dd0d6e4dc2
Masayuki <soy2.san@gmail.com>
2022-12-30 13:07:14 +0900

    arrays and slices

---
+++ arrays_slices/sum.go
@@ -0,0 +1,31 @@
+package arrays_slices
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, nums := range numbersToSum {
+		sums = append(sums, Sum(nums))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, nums := range numbersToSum {
+		if len(nums) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := nums[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
